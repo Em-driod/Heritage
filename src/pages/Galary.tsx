16d98744d43c626182d1179cc7126198a7bbbac6
Em-driod <sheenleen2@gmail.com.com>
2025-08-29 17:22:32 +0100

    sss

--- src/pages/Galary.tsx
+++ src/pages/Galary.tsx
@@ -698,7 +698,7 @@ const Galary = () => {
                 WebkitTextFillColor: 'transparent',
               }}
             >
-              Mexican
+              Mexican   
             </div>
           </div>
           <div className="flex items-center space-x-2 mt-4">
