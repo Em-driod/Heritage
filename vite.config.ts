7408ce901751f83cbc165c827461b6f9396d27bf
Em-driod <sheenleen2@gmail.com.com>
2025-09-08 18:18:32 +0100

    see

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  appType: 'spa',
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(dirname(fileURLToPath(import.meta.url)), 'index.html'),
+        // Add other entry points if needed
+      },
+    },
+  },
 })
